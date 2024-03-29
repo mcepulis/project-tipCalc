c9f20cab63ab7c90acefd7ee3dd8da5cb2cd7d69
mcepulis <109752570+mcepulis@users.noreply.github.com>
2024-03-29 12:44:31 +0200

    fix commit

--- main.js
+++ main.js
@@ -4,18 +4,9 @@ const resultsDisplayDOM = document.getElementById('result');
 const billInputDOM = document.getElementById('bill-input');
 const rateInputDOM = document.getElementById('rate-input');
 
-
-function kainaSuPVM(kainaBePVM){
-    const PVM = 21;
-    const coof = 1 + PVM / 100;
-    const galutineKaina = kainaBePVM * coof;
-    return galutineKaina + ' Eur';
- 
-}
  
 
 checkBtnDOM.addEventListener('click', function () {
-    // let count = 0;
     const bill = billInputDOM.value;
     const rate = rateInputDOM.value;
     console.log(bill, rate);
